a826ff11264ae0b90317ccaccf25c27f27c6dc8d
Liu Sida <yklsdq@163.com>
2021-03-22 23:20:16 +0800

    更新

---
+++ SDBaseClassLib/Classes/Extensions/SDView+UIView.swift
@@ -0,0 +1,32 @@
+//
+//  SDView+UIView.swift
+//  SDBaseClassLib
+//
+//  Created by Liu Sida on 2021/3/22.
+//  Copyright © 2021 奋斗青年. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    @discardableResult
+    func setCornerRadius(_ cornerRaidus: CGFloat, maskToBounds: Bool = true) -> Self {
+        layer.masksToBounds = maskToBounds
+        layer.cornerRadius = cornerRaidus
+        return self
+    }
+    
+    @discardableResult
+    func setBorderWidth(_ borderWidth: CGFloat) -> Self {
+        layer.borderWidth = borderWidth
+        return self
+    }
+    
+    @discardableResult
+    func setBorderColor(_ borderColor: UIColor?) -> Self {
+        layer.borderColor = borderColor?.cgColor
+        return self
+    }
+}
